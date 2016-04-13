01feffb2d5e84c5b3d16f530700a9e7a44c3806a
Klitos Giannakopoulos <klitos@duckseason.mobi>
2016-04-13 13:18:34 +0300

    changed CowGame class to extend Game and start a new GameScreen

---
+++ cow-game-core/src/mobi/duckseason/cowgame/GameScreen.java
@@ -0,0 +1,56 @@
+package mobi.duckseason.cowgame;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+
+public class GameScreen implements Screen {
+
+	public GameScreen() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0.7f, 0.7f, 0.7f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
