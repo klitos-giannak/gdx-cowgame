35d85bd1481cfb2b9bc21bc2d45a2314eefcbc3e
Klitos Giannakopoulos <klitos@duckseason.mobi>
2016-04-14 11:54:20 +0300

    Created classes Bomb and Cow
    Each of these classes create a new kind of Actor.
    We could just use image objects but later we will add code to these classes to help us on our game concept

---
+++ cow-game-core/src/mobi/duckseason/cowgame/Bomb.java
@@ -0,0 +1,12 @@
+package mobi.duckseason.cowgame;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+public class Bomb extends Image {
+
+	public Bomb() {
+		super(new Texture("bomb.png"));
+		
+	}
+}
---
+++ cow-game-core/src/mobi/duckseason/cowgame/Cow.java
@@ -0,0 +1,13 @@
+package mobi.duckseason.cowgame;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+public class Cow extends Image {
+
+	public Cow() {
+		super(new Texture("cow.png"));
+		
+	}
+
+}
