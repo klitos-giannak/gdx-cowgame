f03b5095b17911409b0c05fedaa3c9aadebf8aba
Klitos Giannakopoulos <klitos@duckseason.mobi>
2016-04-14 12:02:58 +0300

    Create an animiated bomb dropping on our screen
    We inroduce Action class.
    Animations are represented by Actions and can be added on Actors

--- cow-game-core/src/mobi/duckseason/cowgame/GameScreen.java
+++ cow-game-core/src/mobi/duckseason/cowgame/GameScreen.java
@@ -1,10 +1,15 @@
 package mobi.duckseason.cowgame;
 
+import java.util.Random;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveByAction;
+import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.ScalingViewport;
@@ -20,9 +25,36 @@ public class GameScreen implements Screen {
 		stage = new Stage(v);
 		Gdx.input.setInputProcessor(stage);
 		
-
+		//test our bomb creation
+		//stage.addActor(createBombActor());
+		
 		//uncomment following line to see the game over image when starting your game
 		//showGameOver();
+	}
+	
+	private Bomb createBombActor() {
+		Bomb bomb = new Bomb();
+
+		//we need the bomb to appear on a random x point inside our stage's viewport
+		//and it's y should be just above our stage's viewport
+		int x = new Random().nextInt( (int) (WIDTH-bomb.getWidth()) );
+		bomb.setPosition(x, HEIGHT);
+		
+		//Calculate the distance our bomb should be travel vertically (drop)
+		float dropDistance = stage.getHeight()+bomb.getHeight();
+		
+		//Create an action to add to our bomb actor with the calculated distance
+		MoveByAction dropAction = Actions.moveBy(0, -dropDistance, 2);
+		
+		//Create a combined action.
+		//After animating the drop, our actor should be removed from stage
+		SequenceAction action = new SequenceAction();
+		action.addAction(dropAction);
+		action.addAction(Actions.removeActor());
+		
+		//finally add the combined action to our bomb-actor and return it
+		bomb.addAction(action);
+		return bomb;
 	}
 	
 	private void showGameOver() {
